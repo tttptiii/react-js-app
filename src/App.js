37dc9726c040af7a3b91f097c2112fa9aad4dcf4
Han Young <ttt.pt.iii@gmail.com>
2022-02-01 14:51:16 +0900

    sidebar skeleton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+
+import { Side } from "./components/Side";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Side></Side>
+        <Routes>
+          <Route path="/about" exact component={App} />
+          <Route path="/a" component={App} />
+          <Route path="/b" component={App} />
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          --- UNDER CONSTRUCTION! ---
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/Side.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../css/side.css";
+import { Link, useLocation } from "react-router-dom";
+
+import { Title, SideItem, Copyright } from "./SideItem";
+
+export const Side = function () {
+  const pathName = useLocation().pathname;
+  const menus = [
+    { name: "about", path: "/about" },
+    { name: "tab-a", path: "/a" },
+    { name: "tab-b", path: "/b" }
+  ];
+  return (
+    <div className="sidebar">
+      <Link to="/">
+        <Title titleString="ttt.pt.iii"></Title>
+      </Link>
+      {menus.map((menu, index) => {
+        return (
+          <Link to={menu.path} key={index}>
+            <SideItem
+              menu={menu}
+              isActive={pathName === menu.path ? true : false}
+            />
+          </Link>
+        );
+      })}
+      <Copyright></Copyright>
+    </div>
+  );
+}
