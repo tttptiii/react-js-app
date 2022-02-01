b9cf36fd1298e3119971c270e400f3e5fac6479c
Han Young <68108832+tttptiii@users.noreply.github.com>
2022-02-01 14:54:45 +0900

    implemented sidebar skeleton (#1)
    * sidebar start
    
    * sidebar skeleton

---
+++ src/components/SideItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Title = function ({ titleString }) {
+  return (
+    <div className="menu-title">
+      <p>{titleString}</p>
+    </div>
+  );
+}
+
+export const SideItem = function ({ menu, isActive }) {
+  return isActive === true ? (
+    <div className="menu-active">
+      <p>{menu.name}</p>
+    </div>
+  ) : (
+    <div className="menu-common">
+      <p>{menu.name}</p>
+    </div>
+  );
+}
+
+export const Copyright = function () {
+  return (
+    <div className="menu-copyright">
+      <p>
+        © 2015-2022. Han Young<br />
+        All contents cannot be copied without permission.
+      </p>
+    </div>
+  );
+}
