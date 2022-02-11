20194c62ec840cc829dce0b4590a17354a9aeafe
Han Young <ttt.pt.iii@gmail.com>
2022-02-12 01:32:45 +0900

    2019

---
+++ src/pages/2019/Sound.js
@@ -0,0 +1,22 @@
+import ttt_logo from '../../assets/images/ttt_white.png';
+
+function Sound(props) {
+  return (
+    <div className="App">
+      <img src={ttt_logo} className="image-style-1" alt="ttt_logo" />
+      <p>
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/564900558&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/577331886&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/583776423&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/590602479&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/617525400&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/624809073&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/638502630&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/695011630&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+        <iframe width="650" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/717614038&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><br />
+      </p>
+    </div>
+  );
+}
+
+export default Sound;
---
+++ src/pages/2019/Spaces.js
@@ -0,0 +1,21 @@
+import img10 from '../../assets/images/2019/spaces/10.png';
+import img11 from '../../assets/images/2019/spaces/11.png';
+import img12 from '../../assets/images/2019/spaces/12.png';
+import img13 from '../../assets/images/2019/spaces/13.jpg';
+import img14 from '../../assets/images/2019/spaces/14.png';
+import img15 from '../../assets/images/2019/spaces/15.png';
+
+function Spaces(props) {
+  return (
+    <div className="App">
+      <img src={img10} className="image-style-1" alt="10" />
+      <img src={img11} className="image-style-1" alt="11" />
+      <img src={img12} className="image-style-1" alt="12" />
+      <img src={img13} className="image-style-1" alt="13" />
+      <img src={img14} className="image-style-1" alt="14" />
+      <img src={img15} className="image-style-1" alt="15" />
+    </div>
+  );
+}
+
+export default Spaces;
