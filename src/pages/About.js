ed559ebf9118df537e9e7bfd7c5385da3a464f6b
Han Young <ttt.pt.iii@gmail.com>
2022-02-13 17:41:38 +0900

    tmp about image

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,9 @@
+import img from '../assets/images/tttptiii.png';
+
 function About(props) {
   return (
     <div className="App">
+      <img src={img} width='350' alt="profile" />
       <p><strong>Han Young</strong> is media artist based in Seoul, Korea, working/experimenting on self initiated<br />
         projects which explore possibilities of real/virtual spaces created with sound, 3D and<br />
         interactive media.</p>
